ff2c936287bfb683437870d520f42a2fc580cca9
lplum <51308441+lplum@users.noreply.github.com>
2019-09-05 19:26:27 +0800

    Add files via upload

---
+++ app/Http/Controllers/wechat/wechatController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class wechatController extends Controller
+{
+    public function get_access_token()
+    {
+    	echo 111;
+    }
+}
